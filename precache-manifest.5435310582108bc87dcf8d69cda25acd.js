0a2a4f05d7486b9698ad7a34b4cd29d7079466f5
jeanwahlung <jeancarloswahlung@gmail.com>
2019-02-09 23:25:09 -0600

    Updates

---
+++ precache-manifest.5435310582108bc87dcf8d69cda25acd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/caja-magica/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "29a09d71d9cb1374eeee",
+    "url": "/caja-magica/static/js/runtime~main.29a09d71.js"
+  },
+  {
+    "revision": "9cf089372f2d3d642a69",
+    "url": "/caja-magica/static/js/main.9cf08937.chunk.js"
+  },
+  {
+    "revision": "fbfa9ac1c04d6f818c50",
+    "url": "/caja-magica/static/js/1.fbfa9ac1.chunk.js"
+  },
+  {
+    "revision": "9cf089372f2d3d642a69",
+    "url": "/caja-magica/static/css/main.b6314285.chunk.css"
+  },
+  {
+    "revision": "26ea9f118475f960c2f4424ca836eb81",
+    "url": "/caja-magica/index.html"
+  }
+];
